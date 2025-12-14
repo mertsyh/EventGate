798f93afe0e076737b6c66ffdb6e29c679a52c24
Eren <ergec93@gmail.com>
2025-12-14 21:18:03 +0300

    Some view models added

---
+++ EventDeneme/Models/ForgotPasswordViewModel.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EventDeneme.Models
+{
+    public class ForgotPasswordViewModel
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+    }
+}
+
---
+++ EventDeneme/Models/LoginViewModel.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EventDeneme.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
+
